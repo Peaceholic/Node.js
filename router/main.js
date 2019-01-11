3ac5c34ccd5c23c3aaebbcfcd717eefd70144fc0
devJWSong <enflwodn@gmail.com>
2019-01-11 14:41:58 +0900

    RESTful API

--- router/main.js
+++ router/main.js
@@ -6,4 +6,111 @@ module.exports = function(app, fs) {
 			length: 5
 		})
 	})
+
+	app.get('/list', function(req, res){
+		fs.readFile(__dirname + "/../data/" + "user.json", 'utf8', function(err, data){
+			console.log(data)
+			res.end(data)
+		});
+	})
+
+	app.get('/getUser/:username', function(req, res){
+		fs.readFile(__dirname + "/../data/" + "user.json", 'utf8', function(err, data){
+			var users = JSON.parse(data)
+			res.json(users[req.params.username])
+		})
+	})
+
+	app.post('/addUser/:username', function(req, res){
+
+		var result = { }
+		var username = req.params.username
+
+		// Check request validity
+		if (!req.body['password'] || !req.body['name']) {
+			result['success'] = 0
+			result['error'] = "invalid request"
+			res.json(result)
+			return
+		}
+
+		// Load data & prevent duplication
+		fs.readFile(__dirname + "/../data/user.json", 'utf8', function(err, data){
+			var users= JSON.parse(data)
+			if (users[username]) {
+				// DUPLICATION FOUND
+				result["success"] = 0
+				result["error"] = "duplicate"
+				res.json(result)
+				return
+			}
+
+			// Add to data
+			users[username] = req.body
+
+			// Save data
+			fs.writeFile(__dirname + "/../data/user.json",
+				JSON.stringify(users, null, '\t'), 'utf8', function(err, data){
+
+				result = {"success": 1}
+				res.json(result)
+			})
+		})
+
+	})
+
+	app.put('/updateUser/:username', function(req, res){
+
+        var result = {  }
+        var username = req.params.username
+
+        // Check request validity
+        if(!req.body["password"] || !req.body["name"]){
+            result["success"] = 0
+            result["error"] = "invalid request"
+            res.json(result)
+            return
+        }
+
+        // Load data
+        fs.readFile( __dirname + "/../data/user.json", 'utf8',  function(err, data){
+            var users = JSON.parse(data)
+            
+            // Add/Modify data
+            users[username] = req.body
+
+            // Save data
+            fs.writeFile(__dirname + "/../data/user.json",
+                         JSON.stringify(users, null, '\t'), "utf8", function(err, data){
+                result = {"success": 1}
+                res.json(result)
+            })
+        })
+    })
+
+	app.delete('/deleteUser/:username', function(req, res){
+		var result = { }
+
+		// Load data
+		fs.readFile(__dirname + "/../data/user.json", 'utf8', function(err, data){
+			var users = JSON.parse(data)
+
+			// If not found
+			if (!users[req.params.username]){
+				result["success"] = 0
+				result["error"] = "not found"
+				res.json(result)
+				return
+			}
+
+			delete users[req.params.username]
+
+			fs.writeFile(__dirname + "/../data/user.json",
+				JSON.stringify(users, null, '\t'), 'utf8', function(err, data){
+					result["success"] = 1
+					res.json(result)
+					return
+			})
+		})
+	})
 }
